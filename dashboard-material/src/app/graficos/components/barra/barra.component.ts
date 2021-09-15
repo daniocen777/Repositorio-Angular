30136fbc9f807779027c9406946a74cadb631cb9
Daniel <danielochoa@outlook.com>
2021-09-15 08:43:11 -0500

    Creando componente barra flexible para usarlo en componente 'barras-dobles'

--- dashboard-material/src/app/graficos/components/barra/barra.component.ts
+++ dashboard-material/src/app/graficos/components/barra/barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Label } from 'ng2-charts';
 import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
 
@@ -8,6 +8,8 @@ import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
   styleUrls: ['./barra.component.scss'],
 })
 export class BarraComponent implements OnInit {
+  @Input() horizontal: boolean = false;
+
   public barChartOptions: ChartOptions = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
@@ -19,26 +21,30 @@ export class BarraComponent implements OnInit {
       },
     },
   };
-  public barChartLabels: Label[] = [
-    '2006',
+  @Input() barChartLabels: Label[] = [
+    /* '2006',
     '2007',
     '2008',
     '2009',
     '2010',
     '2011',
-    '2012',
+    '2012', */
   ];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
 
-  public barChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+  @Input() barChartData: ChartDataSets[] = [
+    /* { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }, */
   ];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.horizontal) {
+      this.barChartType = 'horizontalBar';
+    }
+  }
 
   // events
   public chartClicked({
