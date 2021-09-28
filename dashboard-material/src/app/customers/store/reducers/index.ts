d01779655f7c51ae8715bde0fcc84592b8d1ad61
daniocen777 <danielochoa@outlook.com>
2021-09-28 09:38:29 -0500

    Usando los selectores creando en index de la carpeta 'reducers' para llenar la tabla

--- dashboard-material/src/app/customers/store/reducers/index.ts
+++ dashboard-material/src/app/customers/store/reducers/index.ts
@@ -1,4 +1,5 @@
-import { ActionReducerMap } from '@ngrx/store';
+import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
+import { Customer } from '../../models/customer.model';
 
 import * as fromCustomerReducer from './customer.reducer';
 
@@ -9,3 +10,20 @@ export interface CustomerModuleState {
 export const reducers: ActionReducerMap<CustomerModuleState, any> = {
     customers: fromCustomerReducer.reducer
 }
+
+/* Selectores */
+// Para acceder a todo el estado del módulo "customers"
+export const getState = (state: CustomerModuleState) => state;
+// Para acceder al state "customers"
+export const getCustomersState = createFeatureSelector<fromCustomerReducer.CustomerState>('customers');
+// Para acceder a la propiedad "data" del state "customers" (selector que esta en customer.reducer)
+export const getCustomers = createSelector(getCustomersState, fromCustomerReducer.getCustomers);
+// Para seleccionar un cliente por ID
+export const getCustomerById = (id: number) => createSelector(getCustomers, (customers: Customer[]) => {
+    if (customers) {
+        var customerFound = customers.find(customer => customer.id === id);
+        return customerFound || {};
+    } else {
+        return {};
+    }
+})
