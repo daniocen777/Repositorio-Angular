c1e2f245ac6be11de23e4211b655a5609449b1ce
daniocen777 <danielochoa@outlook.com>
2021-09-29 10:41:39 -0500

    Agregando modulo para usar Menubar, modificando app.component y su modulo

--- pipesPrime/src/app/app.module.ts
+++ pipesPrime/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-/* PrimeModule */
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
+// Modules
+import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PrimeNgModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
