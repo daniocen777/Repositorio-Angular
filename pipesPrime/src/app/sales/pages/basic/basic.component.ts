5e04e513b21c03916b0eb9efcf2c032d2c219ab2
daniocen777 <danielochoa@outlook.com>
2021-09-30 10:12:56 -0500

    Agregando pipes de fechas

--- pipesPrime/src/app/sales/pages/basic/basic.component.ts
+++ pipesPrime/src/app/sales/pages/basic/basic.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-basic',
   templateUrl: './basic.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class BasicComponent implements OnInit {
   nameLower: string = 'danicode';
   nameUpper: string = 'DANICODE';
   name: string = 'danIcoDe';
+
+  date: Date = new Date(); // hoy
 
   constructor() { }
 
