e402b6926ee9242a69078e3c2edd9438300fe8be
daniocen777 <danielochoa@outlook.com>
2021-09-30 10:12:33 -0500

    Cambiando idioma

--- pipesPrime/src/app/app.module.ts
+++ pipesPrime/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 // Modules
@@ -8,7 +8,11 @@ import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* Cambiar locale de la app (idioma) */
+import locale from '@angular/common/locales/es-PE';
+import { registerLocaleData } from '@angular/common/';
 
+registerLocaleData(locale);
 
 @NgModule({
   declarations: [
@@ -18,9 +22,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-     SalesModule
+    SalesModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-PE' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
