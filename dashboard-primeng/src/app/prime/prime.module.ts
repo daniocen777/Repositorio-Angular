7f847dcd0afeacbdcd0c16a103833cfe426217b7
daniocen777 <danielochoa@outlook.com>
2021-10-02 08:41:34 -0500

    Agregando modulo para el avatar

--- dashboard-primeng/src/app/prime/prime.module.ts
+++ dashboard-primeng/src/app/prime/prime.module.ts
@@ -4,8 +4,10 @@ import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 import { CardModule } from 'primeng/card';
 import { PanelMenuModule } from 'primeng/panelmenu';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
 
 @NgModule({
-  exports: [SidebarModule, ButtonModule, MenubarModule, CardModule, PanelMenuModule]
+  exports: [SidebarModule, ButtonModule, MenubarModule, CardModule, PanelMenuModule, AvatarModule, AvatarGroupModule]
 })
 export class PrimeModule { }
