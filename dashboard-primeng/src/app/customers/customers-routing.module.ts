00137ca7ddc7d79f9f1f1bd1bc3aace201157390
daniocen777 <danielochoa@outlook.com>
2021-10-03 10:07:38 -0500

    Quitando ruta anterior 'list-main' para transformarla en pagina

--- dashboard-primeng/src/app/customers/customers-routing.module.ts
+++ dashboard-primeng/src/app/customers/customers-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ListMainComponent } from './components/list-main/list-main.component';
 import { CustomerMainComponent } from './pages/customer-main/customer-main.component';
+import { ListComponent } from './pages/list/list.component';
 
 const routes: Routes = [
   {
     path: '', component: CustomerMainComponent, children: [
       { path: '', pathMatch: 'full', redirectTo: 'list' },
-      { path: 'list', component: ListMainComponent }
+      { path: 'list', component: ListComponent }
     ]
   }
 ];
