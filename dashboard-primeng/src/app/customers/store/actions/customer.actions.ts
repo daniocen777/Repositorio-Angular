42b6322d9475cd03d82b3f394780cfb002cff993
daniocen777 <danielochoa@outlook.com>
2021-10-06 09:35:00 -0500

    Agregando 3 acciones para actualizar cliente

--- dashboard-primeng/src/app/customers/store/actions/customer.actions.ts
+++ dashboard-primeng/src/app/customers/store/actions/customer.actions.ts
@@ -1,12 +1,22 @@
 import { Action } from "@ngrx/store";
 import { Customer } from "../../models/customer.model";
 
+/* Lita */
 export const LOAD_CUSTOMERS = "[Customer] LOAD_CUSTOMERS"
 
-export const LOAD_CUSTOMERS_SUCCESS = "LOAD_CUSTOMERS_SUCCESS"
+export const LOAD_CUSTOMERS_SUCCESS = "[Customer] LOAD_CUSTOMERS_SUCCESS"
 
-export const LOAD_CUSTOMERS_FAIL = "LOAD_CUSTOMERS_FAIL"
+export const LOAD_CUSTOMERS_FAIL = "[Customer] LOAD_CUSTOMERS_FAIL"
 
+/* Upadate */
+export const UPDATE_CUSTOMER = " [Customer] UPDATE_CUSTOMER"
+
+export const UPDATE_CUSTOMER_SUCCESS = " [Customer] UPDATE_CUSTOMER_SUCCESS"
+
+export const UPDATE_CUSTOMER_FAIL = " [Customer] UPDATE_CUSTOMER_FAIL"
+
+
+/* Lista */
 export class LoadCustomers implements Action {
     readonly type = LOAD_CUSTOMERS;
 }
@@ -19,7 +29,23 @@ export class LoadCustomersSuccess implements Action {
 export class LoadCustomersFail implements Action {
     readonly type = LOAD_CUSTOMERS_FAIL;
     constructor(public payload: any) { }
-
 }
 
-export type CustomerActions = LoadCustomers | LoadCustomersSuccess | LoadCustomersFail;
+/* Update */
+export class UpdateCustomer implements Action {
+    readonly type = UPDATE_CUSTOMER;
+    constructor(public payload: Customer) { }
+}
+
+export class UpdateCustomerSuccess implements Action {
+    readonly type = UPDATE_CUSTOMER_SUCCESS;
+    constructor(public payload: any) { }
+}
+
+export class UpdateCustomerFail implements Action {
+    readonly type = UPDATE_CUSTOMER_FAIL;
+    constructor(public payload: any) { }
+}
+
+export type CustomerActions = LoadCustomers | LoadCustomersSuccess |
+    LoadCustomersFail | UpdateCustomer | UpdateCustomerSuccess | UpdateCustomerFail;
