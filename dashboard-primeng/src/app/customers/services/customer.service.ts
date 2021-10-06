e1fbafc832b0080232f7bdef51b00f03d1983b07
daniocen777 <danielochoa@outlook.com>
2021-10-06 09:35:38 -0500

    Agregando metodo para actualizar cliente

--- dashboard-primeng/src/app/customers/services/customer.service.ts
+++ dashboard-primeng/src/app/customers/services/customer.service.ts
@@ -1,16 +1,27 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Customer } from '../models/customer.model';
 import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  private _apiUrl = 'http://localhost:3000/usuarios';
+  public httpHeader = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json, text/plain'
+    })
+  };
 
   constructor(private _http: HttpClient) { }
 
   getCustomers(): Observable<Customer[]> {
-    return this._http.get<Customer[]>(`http://localhost:3000/usuarios`);
+    return this._http.get<Customer[]>(`${this._apiUrl}`);
+  }
+
+  update(customer: Customer): Observable<any> {
+    return this._http.put(`${this._apiUrl}/${customer.id}`, JSON.stringify(customer), this.httpHeader);
   }
 }
