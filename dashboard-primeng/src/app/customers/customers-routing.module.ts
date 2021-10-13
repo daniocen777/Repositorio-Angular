539a96b8bb1f1fcf0df3dbfb94192a691f34bccb
daniocen777 <danielochoa@outlook.com>
2021-10-13 10:15:45 -0500

    Creando componente (blank) de prueba para trabajar con dos columnas, una para listar y otra para mostrar información

--- dashboard-primeng/src/app/customers/customers-routing.module.ts
+++ dashboard-primeng/src/app/customers/customers-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlankComponent } from './pages/blank/blank.component';
 import { CustomerMainComponent } from './pages/customer-main/customer-main.component';
 import { ListComponent } from './pages/list/list.component';
 
@@ -7,7 +8,8 @@ const routes: Routes = [
   {
     path: '', component: CustomerMainComponent, children: [
       { path: '', pathMatch: 'full', redirectTo: 'list' },
-      { path: 'list', component: ListComponent }
+      { path: 'list', component: ListComponent },
+      { path: 'blank', component: BlankComponent }
     ]
   }
 ];
--- dashboard-primeng/src/app/customers/customers.module.ts
+++ dashboard-primeng/src/app/customers/customers.module.ts
@@ -14,12 +14,14 @@ import { SharedModule } from '../shared/shared.module';
 import { PrimeModule } from '../prime/prime.module';
 import { ListComponent } from './pages/list/list.component';
 import { CreateComponent } from './pages/create/create.component';
+import { BlankComponent } from './pages/blank/blank.component';
 
 @NgModule({
   declarations: [
     CustomerMainComponent,
     ListComponent,
-    CreateComponent
+    CreateComponent,
+    BlankComponent
   ],
   imports: [CommonModule, ReactiveFormsModule, CustomersRoutingModule, 
     PrimeModule, SharedModule,
