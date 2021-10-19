0b44a06c4e03a04a0ef27f52bbde0235f21c7a5c
daniocen777 <danielochoa@outlook.com>
2021-10-19 10:23:19 -0500

    Agregando store para la lista de clientes

--- dashboard-primeng/src/app/customers/pages/blank/blank.component.ts
+++ dashboard-primeng/src/app/customers/pages/blank/blank.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Customer } from '../../models/customer.model';
+
+import { Store } from '@ngrx/store';
+
+import * as fromStore from '../../store';
 
 @Component({
   selector: 'app-blank',
   templateUrl: './blank.component.html',
   styleUrls: ['./blank.component.scss']
 })
-export class BlankComponent implements OnInit {
+export class BlankComponent implements OnInit, OnDestroy {
+  customers: Customer[] = [];
+  selectedCustomer: Customer = {};
+  private customerSubscription: Subscription = new Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private _store: Store<fromStore.CustomerModuleState>) {
+    this.customerSubscription = this._store.select(fromStore.getCustomers).subscribe((customers: Customer[]) => {
+      this.customers = customers;
+    });
   }
 
+  ngOnInit(): void {
+    this._store.dispatch(new fromStore.LoadCustomers());
+  }
+
+  ngOnDestroy(): void {
+    this.customerSubscription.unsubscribe();
+  }
+
+  showCustomer(customer: Customer): void {
+    this.selectedCustomer = customer;
+    console.log(this.selectedCustomer);
+  }
 }
